6cdfb4311531afb6e783cf510400e5972cad3d44
Hideki Abe <hideki.abe.0630@gmail.com>
2022-01-05 17:43:44 +0900

    サイトロゴ表出コードの変更

--- wp-content/themes/sample-child/template-parts/sitelogo.php
+++ wp-content/themes/sample-child/template-parts/sitelogo.php
@@ -14,9 +14,9 @@
 			'height' => array(),
 		),
 	);
-	// echo( wp_kses( get_custom_logo(), $allowed_logo_html ) );
+	echo( wp_kses( get_custom_logo(), $allowed_logo_html ) );
 	?>
-	<a href="/">Skyward Design Logo</a>
+	<!-- <a href="/">Skyward Design Logo</a> -->
 </div>
 <p>子テーマを使用し、サイト毎に<code>template-parts/sitelogo.php</code>を書き換えてロゴが変更できます。<br>
-外観＞カスタマイズ＞サイト基本情報にロゴを設定し、<code>get_custom_logo()</code>を使用する方法もあるようです。</p>
+外観＞カスタマイズ＞サイト基本情報にロゴを設定し、<code>get_custom_logo()</code>を使用する方法もあり、ここでは<code>get_custom_logo()</code>を使用して表示しています。</p>
